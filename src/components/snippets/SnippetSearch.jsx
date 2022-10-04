67372f698b752c709475cfae5e16a1b1a3a9adb1
tyrkinn <nikemad61@gmail.com>
2022-10-04 16:10:56 +0300

    feat: add CategoryDropdown to SnippetSearch

--- src/components/snippets/SnippetSearch.jsx
+++ src/components/snippets/SnippetSearch.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { Input, Flex, Button } from '@chakra-ui/react'
+import React from "react";
+import { Input, Flex, Button } from "@chakra-ui/react";
+import { CategoryDropdown } from "../shared";
+import { useStore } from "../../store";
 
 const SnippetSearch = () => {
-    return (        
-        <Input placeholder='Search snippet' /> 
-    )
-}
+  const { categoryList, setSearchCategory, searchCategory } = useStore((state) => ({
+    categoryList: state.categoryList,
+    setSearchCategory: state.setSearchCategory,
+    searchCategory: state.searchCategory,
+  }));
+  return (
+    <>
+      <CategoryDropdown categories={categoryList} setCategory={setSearchCategory} currentCategory={searchCategory} />
+      <Input placeholder="Search snippet" />
+    </>
+  );
+};
 
-export default SnippetSearch
+export default SnippetSearch;
