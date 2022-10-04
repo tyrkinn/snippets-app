5a47b3041f48ffdaae373f0df1052831450459de
tyrkinn <nikemad61@gmail.com>
2022-10-04 16:32:59 +0300

    fix: remove defaultValue in Select

--- src/components/shared/CategoryDropdown.jsx
+++ src/components/shared/CategoryDropdown.jsx
@@ -3,7 +3,7 @@ import { Select } from "@chakra-ui/react";
 
 const CategoryDropdown = ({ categories, setCategory, currentCategory }) => {
   return (
-    <Select placeholder="Category" w="30%" defaultValue='none'>
+    <Select placeholder="Category" w="30%">
       {categories.map((c) => (
         <option value={c.name} selected={c.name === currentCategory} key={c.id} onClick={() => setCategory(c)}>
           {c.name}
