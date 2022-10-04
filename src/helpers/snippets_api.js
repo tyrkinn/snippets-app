69cc4109f7fccbf0a07ff6e5ab53d7e66a3147a9
tyrkinn <nikemad61@gmail.com>
2022-10-04 16:10:14 +0300

    fix: id field in getAll functions

--- src/helpers/snippets_api.js
+++ src/helpers/snippets_api.js
@@ -4,7 +4,7 @@ import firestore from "./firebase";
 export const getAllSnippets = async () => {
   const coll = collection(firestore, "snippets");
   const snapshot = await getDocs(coll);
-  const snippets = snapshot.docs.map((d) => d.data);
+  const snippets = snapshot.docs.map((d) => ({...d.data(), id: d.id}));
   return snippets;
 };
 
@@ -16,4 +16,21 @@ export const addSnippet = async (snippet) => {
   } catch {
     return false;
   }
+};
+
+export const addCategory = async (category) => {
+  const coll = collection(firestore, 'categories');
+  try {
+    await addDoc(coll, category)
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+export const getAllCategories = async () => {
+  const coll = collection(firestore, "categories");
+  const snapshot = await getDocs(coll);
+  const categories = snapshot.docs.map((d) => ({...d.data(), id: d.id }));
+  return categories;
 };
