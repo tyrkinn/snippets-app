65ccf476025110bbc2335191723bc30e929cf0cf
tyrkinn <nikemad61@gmail.com>
2022-10-04 16:09:51 +0300

    feat: update store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,30 @@
 import create from "zustand";
+import { addCategory as apiAddCategory, getAllCategories, getAllSnippets } from '../helpers/snippets_api';
 
-export const useStore = create(() => ({
+export const useStore = create((set, get) => ({
   searchCategory: null,
   editorCategory: null,
   categoryList: [],
+  snippetList: [],
+  searchString: '',
+  setSnippetList: async () => {
+    const snippets = await getAllSnippets();
+    set(() => ({ snippetsList: snippets }))
+  },
+  setSearchString: (newSearch) => set(() => ({ searchString: newSearch })),
+  setSearchCategory: (newSearchCategory) => set(() => ({ searchCategory: newSearchCategory})),
+  setEditorCategory: (newEditorCategory) => set(() => ({ editorCategory: newEditorCategory})),
+  setCategoryList: async () => {
+    const categories = await getAllCategories();
+    return set(() => ({categoryList: categories}))
+  },
+  addCategory: async (category) => {
+    await apiAddCategory(category);
+  },
+  filteredSnippets: () => {
+    const { searchString, snippetList } = get();
+    return searchString.trim().length > 2 
+          ? snippetList.filter(s => s.startsWith(searchString.trim()))
+          : snippetList
+  }
 }));
