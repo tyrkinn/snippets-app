289333e821a2cf5c1f7481e36bc2d9b6eaf3bb0d
tyrkinn <nikemad61@gmail.com>
2022-10-05 20:58:07 +0300

    feat: some style changes

--- src/components/category/CategoryItem.jsx
+++ src/components/category/CategoryItem.jsx
@@ -1,24 +1,38 @@
-import { HStack, Text, } from '@chakra-ui/react';
-import { useStore } from '../../store';
-
+import { HStack, Text, useToast, Button, ButtonGroup } from "@chakra-ui/react";
+import { useStore } from "../../store";
 
 export const CategoryItem = ({ item }) => {
-  const setSearchCategory = useStore(state => state.setSearchCategory);
 
+  const toast = useToast({position: 'top-right', isClosable: true})
+  const { updateCategoryList, deleteCategory } = useStore(state => ({updateCategoryList: state.updateCategoryList, deleteCategory: state.deleteCategory}));
+
+  const onDelete = async () => {
+    const isDel = await deleteCategory(item.id);
+    if (isDel) {
+      toast({ status: 'success', title: 'Successfully deleted category'});
+      updateCategoryList();
+    } else {
+      toast({ status: 'error', title: "Can't delete category. Error occured"});
+    }
+  }
+
+ 
   return (
-      <HStack
-        rounded="lg"
-        px={5}
-        py={3}
-        justifyContent="space-between"
-        w="full"
-        _hover={{ background: "gray.200" }}
-        onClick={() => setSearchCategory(item)}
-      >
-        <Text fontSize="md">
-          {item.name}
-        </Text>
-      </HStack>
-  )  
+    <HStack
+      rounded="lg"
+      px={5}
+      py={3}
+      justifyContent="space-between"
+      w="full"
+      _hover={{ background: "gray.100" }}
+      alignItems="center"
+    >
+      <Text fontSize="md" >{item.name}</Text>
+      <ButtonGroup>
+        <Button size="sm" variant="link" colorScheme='telegram' onClick={onDelete}>Delete</Button>
+      </ButtonGroup>
+    </HStack>
+  );
 
-}
+};
+
--- src/components/category/CategoryList.jsx
+++ src/components/category/CategoryList.jsx
@@ -5,19 +5,28 @@ import { CategoryAddField } from "./CategoryAddField";
 import { CategoryItem } from "./CategoryItem";
 
 const CategoryList = () => {
-
-  const categoryList = useStore(state => state.categoryList);
+  const categoryList = useStore((state) => state.categoryList);
 
   return (
-    <Grid w='full' h='full' bgColor='white' rounded='lg' p={5} templateColumns='1fr' templateRows='1fr 1fr 7fr'>
+    <Grid
+      w="full"
+      h="full"
+      shadow="md"
+      bgColor="white"
+      rounded="lg"
+      p={5}
+      templateColumns="1fr"
+      templateRows="1fr 1fr 7fr"
+      gap={5}
+    >
       <GridItem>
-        <Heading size='md'>Categories</Heading>
+        <Heading size="md">Categories</Heading>
       </GridItem>
       <GridItem>
         <CategoryAddField />
       </GridItem>
       <GridItem>
-        <List>
+        <List overflowY="scroll" maxH="400px">
           {categoryList.map((c) => (
             <ListItem key={c.id}>
               <CategoryItem item={c} />
