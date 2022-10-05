e36828818244a36f4a2f361874eee9e6c452344f
tyrkinn <nikemad61@gmail.com>
2022-10-05 20:54:28 +0300

    feat: add copyText helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,14 @@
 export { getAllSnippets, addSnippet } from "./snippets_api";
 
 export { snippetsToCategories } from "./snippets";
+
+export const copyText = async (text) => {
+  try {
+    await navigator.clipboard.writeText(text);
+    return true;
+  } catch(e) {
+    console.error(e);
+    return false;
+  }
+
+}
