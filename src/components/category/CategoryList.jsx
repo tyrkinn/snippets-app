963400f76a1e35224c628337155d82a0bd0799f7
tyrkinn <nikemad61@gmail.com>
2022-10-05 17:01:40 +0300

    feat: rebuild CategoryList

--- src/components/category/CategoryList.jsx
+++ src/components/category/CategoryList.jsx
@@ -1,13 +1,31 @@
 import React from "react";
-import { List, ListItem } from "@chakra-ui/react";
+import { List, ListItem, Grid, GridItem, Heading } from "@chakra-ui/react";
+import { useStore } from "../../store";
+import { CategoryAddField } from "./CategoryAddField";
+import { CategoryItem } from "./CategoryItem";
 
-const CategoryList = ({ categories }) => {
+const CategoryList = () => {
+
+  const categoryList = useStore(state => state.categoryList);
+
   return (
-    <List>
-      {categories.map((c) => (
-        <ListItem key={c.id}>{c.name}</ListItem>
-      ))}
-    </List>
+    <Grid w='full' h='full' bgColor='white' rounded='lg' p={5} templateColumns='1fr' templateRows='1fr 1fr 7fr'>
+      <GridItem>
+        <Heading size='md'>Categories</Heading>
+      </GridItem>
+      <GridItem>
+        <CategoryAddField />
+      </GridItem>
+      <GridItem>
+        <List>
+          {categoryList.map((c) => (
+            <ListItem key={c.id}>
+              <CategoryItem item={c} />
+            </ListItem>
+          ))}
+        </List>
+      </GridItem>
+    </Grid>
   );
 };
 
